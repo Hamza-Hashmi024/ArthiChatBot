8ee21a6894455c7a6a1538713ff3077fe0a25d97
Hamza-Hashmi024 <hamza064@gmail.com>
2025-09-02 01:10:17 +0500

    Conect it to the frontend

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express = require("express");
 const dotenv = require("dotenv");
 const chatRouter = require("./routes/chat");
 const pool = require("./config/db");
+const cors = require("cors");
 
 
 dotenv.config();
 
 const app = express();
+
+
 app.use(express.json());
 
-
-
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST"]
+}));
 app.use("/api", chatRouter);
 // Test Route
 app.get("/", (req, res) => {
